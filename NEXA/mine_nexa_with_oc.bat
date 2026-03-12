bf202a1c70fa96a670e7873d068faf9dc5c5dc4e
DimebagULKU <denizulku2011@gmail.com>
2026-03-13 01:51:30 +0300

    Add files via upload

--- NEXA/mine_nexa_with_oc.bat
+++ NEXA/mine_nexa_with_oc.bat
@@ -4,9 +4,9 @@ cd /d %~dp0
 REM ########## Kullanıcı Ayarları ##########
 set "POOL=stratum+tcp://nexa.2miners.com:5050"
 set "WALLET=nexa:nqtsq5g5l2vrxqt4wydwx7d7w4m20dt5y36pe72xyynu049s"
-set "CORELOCK=1450"
-set "MEMLOCK=5001"
-set "COREOFFSET=150"
+set "CORELOCK=400"
+set "MEMLOCK=1500"
+set "COREOFFSET=0"
 set "EXTRAPARAMETERS=--apiport 8020"
 REM ########## Ayarlar Bitti ##########
 
---
+++ NEXA/run_hidden.txt
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "mine_nexa_with_oc", 0, False
